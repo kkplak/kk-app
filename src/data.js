69c53fd42c7824378ac18b257082374a1eeb7340
Konrad <kkplak@gmail.com>
2022-01-24 12:56:11 +0100

    update 24.01
    testimonials added

--- src/data.js
+++ src/data.js
@@ -20,7 +20,7 @@ export const projects = [
     subtitle: "React and Firebase",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebflow.com%2Ftemplates%2Fhtml%2Fcrypto-app-website-template&psig=AOvVaw25Ody0_IOF362VGeB1meeo&ust=1643111419642000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLCp1ZapyvUCFQAAAAAdAAAAABAD",
     link: "https://github.com/kkplak",
   },
   {
@@ -28,9 +28,19 @@ export const projects = [
     subtitle: "React Hooks",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwebflow.com%2Ftemplates%2Fhtml%2Fcrypto-app-website-template&psig=AOvVaw25Ody0_IOF362VGeB1meeo&ust=1643111419642000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCLCp1ZapyvUCFQAAAAAdAAAAABAD",
     link: "https://github.com/kkplak",
   },
 ];
 
 export const skills = ['React.js','JavaScript','HTML5 & CSS3','Tailwind CSS','Git & GitHub', 'Figma', 'Adobe Creative Cloud', 'DaVinci Resolve']
+
+
+export const testimonials = [
+  {
+    quote: "Konrad is a highly driven and goal oriented person who usually goes the extra miles to achieve it what he envisions. Brave and youthful , self explanatory when you have the chance to connect and speak to him over tea, about his journey thus far even at a tender age. Glad to have worked with you!",
+    name: "Fatin Zulkefly",
+    image: "https://media-exp1.licdn.com/dms/image/C5103AQG8mkO6AoAQRg/profile-displayphoto-shrink_800_800/0/1544436794221?e=1648684800&v=beta&t=6T76gDAuHp5x85Nome458kE9PaRjogUhyKNJiun9XHE",
+    company: "Interior Design & Styling"
+  }
+]
