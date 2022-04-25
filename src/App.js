2cf8b92b9787e14fe6652b3fa610832c00cf9d07
Konrad <kkplak@gmail.com>
2022-04-25 14:56:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Contact from './components/Contact'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom';
-
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
